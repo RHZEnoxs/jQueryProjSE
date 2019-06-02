41a7eb2cde47719990580e09046539b84f444385
ENOXS-PC\Enoxs <rhxs020@gmail.com>
2019-06-02 20:50:49 +0800

    add: Markdown Convert 1. Checkbox 2. Table #20160602001

--- src/Markdown/markdownScript.js
+++ src/Markdown/markdownScript.js
@@ -1,7 +1,12 @@
 $(function () {
     var $show = $('#show');
     var $convertBtn = $('#convertBtn');
+    var $convertBtn2 = $('#convertBtn2');
+    
     var converter = new showdown.Converter();
+    converter.setOption('tables', true); // 開啟表格功能
+    converter.setOption('tasklists', true); // 開啟 checkbox 功能
+    
     var text = '# hello, markdown!';
     var html = converter.makeHtml(text);
     $show.html(html);
@@ -12,6 +17,26 @@ $(function () {
             $show.html(html);
         }).always(function () {});
     });
-
+    $convertBtn2.off('click').on('click', function () {
+        var input =
+            '# H1 標題 \n' +
+            '## H2 標題 \n' +
+            '### H3 標題 \n' +
+            '#### H4 標題 \n' +
+            '##### H5 標題 \n' +
+            '###### H6 標題 \n' +
+            '*斜體* \n\n' +
+            '**粗體** \n' +
+            '- [ ] checkbox 1 \n' +
+            '- [x] checkbox 2 \n\n' +
+            '[Makee.io Blog](http://oranwind.org/) \n\n' +
+            '![Makee.io logo] (http://makee.io/manual/img/makee_logo.jpg) \n\n' +
+            '| 表格 1 | 表格 2 |\n' +
+            '|======= |=======|\n' +
+            '| 表格 3 | 表格 4 |\n' +
+            '| 表格 5 | 表格 6 |\n';
+        var html = converter.makeHtml(input);
+        $show.html(html);
+    });
 
 });
